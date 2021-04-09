e0ec6c95babf8f5d198312722e060cf2237681f1
sujin7837 <sujin7837@gmail.com>
2021-04-09 12:23:05 +0900

    Initial Commit

---
+++ 두 개 뽑아서 더하기/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  두 개 뽑아서 더하기
+//
+//  Created by 김수진 on 2021/04/09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
